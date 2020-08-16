eb9281b06724b751dea4632e27f6559a9ee14989
Aslam, Omair <Omair.Aslam@tabcorp.com.au>
2020-08-16 10:37:47 +1000

    doing a text changes using a git branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> ohh shut up
+          This text was changes using a git branch, merged to master
         </p>
         <a
           className="App-link"
